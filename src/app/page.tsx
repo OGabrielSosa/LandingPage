ea88f23dfcdff60b451198e7ad22d9b35303a40f
Matias Daniel Alessandrello <matialess@gmail.com>
2025-07-20 16:30:46 -0300

    se crea el componente Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import Desplegable from "@/components/Desplegable";
+import Button from "@/components/Button";
 import Input from "@/ui/Input";
 import Textarea from "@/ui/Textarea";
 
@@ -16,6 +17,8 @@ export default function Home() {
         <br />
         <br />
         <Textarea/>
+        <p>Button</p>
+        <Button/>
 
       </div>
     </main>
