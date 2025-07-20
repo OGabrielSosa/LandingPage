96290ca0eb98e1502fc28b308f95dbddb595ec26
Dario <dgimenez.developer@gmail.com>
2025-07-19 23:43:49 -0300

    Añadido componente Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import Desplegable from "@/components/Desplegable";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen">
       <div className="container mx-auto px-6 py-24">
         <Desplegable />
         <HeroSection />
         <AboutSection />
+        <Footer />
 
       </div>
     </main>
