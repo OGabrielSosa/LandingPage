3c39dc3659991823c0df1acf13e2b6b0851545ba
FolkodeGroup <contactofolkode@gmail.com>
2025-07-23 00:43:34 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <QueHacemosSection />
+        <Tecnologias />
         <p>Input y Textarea</p>
         <Input/>
         <br />
