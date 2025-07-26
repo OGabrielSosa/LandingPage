f56569ba3ddf10d3a5b9fcdbe93d8662ae3d73de
FedericoPaal <122946141+FedericoPaal@users.noreply.github.com>
2025-07-26 12:48:43 -0300

    Refactor/proy clientes (#9)
    * Se hace mas grande y se refactoriza las cards de los Proyectos de Clientes
    
    * Se hace un tanto mas responsivo pero no del todo a Proyectos de Clientes
    
    * Se mejora casi del todo el responsive de Proyectos de Clientes
    
    * Se termina de hacer el responsive de Proyectos de Clientes
    
    * Ultimos cambios a Proyectos de Clientes
    
    * Se arregla y se hace merge de problemas

--- src/components/ProyectoClientes.tsx
+++ src/components/ProyectoClientes.tsx
@@ -7,40 +7,40 @@ import Image from 'next/image';
 
 export default function ProyClientes(){
     const proyectos = [
-        { image: Andet, title: 'Proyecto 1', description: 'Descripción breve del proyecto 1' },
-        { image: Autopartes, title: 'Proyecto 2', description: 'Descripción breve del proyecto 2' },
-        { image: Luminova, title: 'Proyecto 3', description: 'Descripción breve del proyecto 3' },
-        { image: Revisteria, title: 'Proyecto 4', description: 'Descripción breve del proyecto 4' }
+        { image: Andet, title: 'Andet', description: 'E-commerce de electrodomésticos'},
+        { image: Autopartes, title: 'Autopartes', description: 'Software Empresarial de autopartes'},
+        { image: Luminova, title: 'Luminova', description: 'Software Empresarial de luminarias'},
+        { image: Revisteria, title: 'La Revisteria', description: 'E-commerce de libros y cómics'}
     ];
     return (
-        <section className="text-inverse section-proy-clientes">
-            <h1 className="text-h1 mb-8 text-center">
-                Proyectos de Nuestros Clientes
-            </h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 div-clientes">
-                {proyectos.map((proyecto, index) => (
-                    <div key={index} className='card-clientes'>
-                        <div className='img-clientes'>
-                          <Image
-                            src={proyecto.image}
-                            alt={proyecto.title}
-                            width={proyecto.image.width}
-                            height={proyecto.image.height}
-                            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                            priority
-                          />
+            <section className="text-inverse section-proy-clientes">
+                <h1 className="text-h1 mb-8 text-center">
+                    Proyectos de Nuestros Clientes
+                </h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 div-clientes">
+                    {proyectos.map((proyecto, index) => (
+                        <div key={index} className='card-clientes'>
+                            <div className='img-clientes'>
+                            <Image
+                                src={proyecto.image}
+                                alt={proyecto.title}
+                                width={proyecto.image.width}
+                                height={proyecto.image.height}
+                                style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                                priority
+                            />
+                            </div>
+                            <div className='text-clientes '>
+                                <h3 className="text-2xl font-bold mt-4">{proyecto.title}</h3>
+                                <p className="text-md">{proyecto.description}</p>
+                                <button className="mt-4 px-6 py-2 bg-primary text-white rounded-lg hover:bg-primary-hover transition-colors button-clientes">
+                                    Ver
+                                </button>
+                            </div>
                         </div>
-                        <div className='text-clientes'>
-                            <h3 className="text-2xl font-bold mt-4">{proyecto.title}</h3>
-                            <p className="text-md">{proyecto.description}</p>
-                            <button className="mt-4 px-6 py-2 bg-primary text-white rounded-lg hover:bg-primary-hover transition-colors button-clientes">
-                                Ver
-                            </button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </section>
-    )
+                    ))}
+                </div>
+            </section>
+        )
 
-}
+    }
