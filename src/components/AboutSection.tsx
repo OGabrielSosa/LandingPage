778d020bb5d472cb2a6199ad51a78315c076d7f9
Dario Gimenez <dgimenez.developer@gmail.com>
2025-07-30 11:47:42 -0300

    Corregido img -> Image (#14)

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 export default function AboutSection() {
   return (
@@ -11,20 +12,25 @@ export default function AboutSection() {
           <div className="static md:absolute left-20 md:left-40 lg:left-0 -top-1 w-40 h-16 xl:left-160 md:w-30 md:h-30 lg:w-64 lg:h-24 xl:w-20 xl:h-32 bg-[#01454F] z-0" />
           {/* Cuadrado verde detrás abajo */}
           <div className="static md:absolute left-20 top-10 w-20 h-20 md:w-20 md:h-70 bg-[#5E8D6B] z-0" />
-          {/* Imagen secundaria (Compu) */}
-          <img
+          <Image
             src="/Compu.png"
             alt="Código"
-            className="static md:absolute md:left-10 md:top-5 w-24 h-32 md:w-60 md:h-56 lg:w-64 lg:h-32 xl:w-80 xl:h-40 2xl:w-96 2xl:h-48 object-cover rounded-lg shadow-lg z-20 border-4 border-black" />
+            width={384}
+            height={192}
+            className="static md:absolute md:left-10 md:top-5 w-24 h-32 md:w-60 md:h-56 lg:w-64 lg:h-32 xl:w-80 xl:h-40 2xl:w-96 2xl:h-48 object-cover rounded-lg shadow-lg z-20 border-4 border-black"
+            priority
+          />
           {/*  className="static md:absolute left-0 top-4 w-24 h-32 md:w-60 md:h-52 lg:w-160 lg:h-65 object-cover rounded-lg shadow-lg z-20 border-4 border-black" */}
 
 
           {/* Imagen principal (ChicoOficina) */}
-          <img
+          <Image
             src="/ChicoOficina.png"
             alt="Trabajo en equipo"
+            width={384}
+            height={192}
             className="static md:absolute md:left-30 md:top-20 w-32 h-32 md:w-60 md:h-56 lg:w-64 lg:h-32 xl:w-80 xl:h-40 2xl:w-96 2xl:h-48 object-cover rounded-lg shadow-lg z-20 border-4 border-black"
-          /* className="static md:absolute left-30 top-20 w-24 h-32 md:w-60 md:h-55 lg:w-64 lg:h-32 xl:w-80 xl:h-40 2xl:w-96 2xl:h-48 object-cover rounded-lg shadow-lg z-20 border-4 border-black" */
+            priority
           />
         </div>
         {/* Texto a la derecha */}
